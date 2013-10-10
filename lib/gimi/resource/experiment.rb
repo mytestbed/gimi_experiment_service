7dc787ee8ca9a81939392a07ca46c8bb9bf28f65
Jack C Hong <jhong@nicta.com.au>
2013-10-10 16:57:32 +1100

    First attempt to create irods folders upon creation of exp

--- lib/gimi/resource/experiment.rb
+++ lib/gimi/resource/experiment.rb
@@ -3,20 +3,37 @@ require 'omf-sfa/resource/oresource'
 require 'omf-sfa/resource/project'
 
 module GIMI::Resource
-
-  # This class represents a user in the system.
-  #
   class Experiment < OMF::SFA::Resource::OResource
-    #oproperty :iticket, :model => GIMI::Resource::ITicket
-    has 1, :iticket, :model => GIMI::Resource::ITicket
-    has n, :slices, :model => GIMI::Resource::Slice
     belongs_to :project, OMF::SFA::Resource::Project, :required => false
+    oproperty :path, String
+
+    after :create do |exp|
+      exp.path = "/#{self.project.name}/#{self.name}/" unless exp.project.nil?
+      exp.save
+      info "After save: write to irods: #{exp.path}"
+      begin
+        `imkdir -p #{exp.path}`
+      rescue => e
+        error e.message
+        debug e.backtrace.join("\n")
+      end
+    end
 
     def to_hash_long(h, objs, opts = {})
       super
-      h[:project] = self.project.to_hash(objs, opts)
-      h[:slices] = self.slices.map {|s| s.to_hash(objs, opts)}
-      h[:iticket] = self.iticket.to_hash(objs, opts) if self.iticket
+      if self.project
+        h[:project] = self.project.to_hash_brief(opts)
+        h[:path] = self.path
+      end
+      h
+    end
+
+    def to_hash_brief(opts = {})
+      h = super
+      if self.project
+        h[:project_name] = self.project.name
+        h[:path] = self.path
+      end
       h
     end
   end # classs
@@ -24,7 +41,6 @@ end # module
 
 # Extend Project with Experiments
 module OMF::SFA::Resource
-
   # This class represents a Project which is strictly connected to the notion of the Slice/Account
   #
   class Project < OResource
